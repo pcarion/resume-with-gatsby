24a0f4ad412a2acd7eb5f0da5e5437422fb0f8c6
Pierre Carion <pcarion@gmail.com>
2020-01-19 09:18:59 -0800

    basic version

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,25 @@
 import React from 'react';
 
-export default () => <h1>Hello from gatsby</h1>;
+const Resume = ({ data }) => {
+  const resume = data.resumeYaml;
+  const { basics } = resume;
+  return (
+    <React.Fragment>
+      <h1>{basics.name}</h1>
+      <h2>{basics.email}</h2>
+    </React.Fragment>
+  );
+};
+
+export default Resume;
+
+export const query = graphql`
+  query MyQuery {
+    resumeYaml {
+      basics {
+        email
+        name
+      }
+    }
+  }
+`;
